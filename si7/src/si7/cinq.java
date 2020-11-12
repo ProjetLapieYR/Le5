d8e7229f221f373f294d5dacf0a4c38b5d987657
yrodach2019 <yrodach2019@localhost>
2020-11-12 14:41:57 +0100

    test

---
+++ si7/src/si7/cinq.java
@@ -0,0 +1,28 @@
+package si7;
+
+
+		
+		public class cinq {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				
+				
+				
+				
+				System.out.println(" ______"+"\n"+"|"+"\n"+"|"+"______"+"\n"+"       |"+"\n"+"       |");
+				System.out.println(" ______");
+		                
+		                  
+							
+							
+							
+					     
+				;
+			}
+
+		
+
+	}
+
+
